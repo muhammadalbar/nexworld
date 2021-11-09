bfc71cc7ef67ba088a4c278d5aad9b51c7383919
oneight2 <syarif.cibatu@gmail.com>
2021-11-09 13:56:22 +0700

    fix auth fb

--- routes/signinFacebook.js
+++ routes/signinFacebook.js
@@ -14,8 +14,8 @@ router.get("/dashFacebook", isLoggedIn, async (req, res) => {
     //EMAIL VALIDATION
     // const user = req.user;
     // res.send({ email, name });
-    let getemail = req.user.emails.map(({ value }) => value);
-    let email = getemail[0];
+    let getemail = req.user.emails[0];
+    let email = getemail.value;
     let props = { name: req.user.displayName };
     let uid = uuidv4();
     let register_date = new Date();
