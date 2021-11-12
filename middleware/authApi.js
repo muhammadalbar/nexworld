7dfc72185b77eda742f7f477b7efa9d855cd393d
oneight2 <syarif.cibatu@gmail.com>
2021-11-12 11:43:21 +0700

    fix authApi

--- middleware/authApi.js
+++ middleware/authApi.js
@@ -3,14 +3,15 @@ const db = require("../db/db");
 module.exports = {
   isAllow: async (req, res, next) => {
     const api_key = req.headers.api_key;
+    const user = "admin";
     const admin = await db.query(
-      `SELECT * FROM users WHERE uid = $1 AND role = 'admin'`,
-      [api_key]
+      `SELECT * FROM users WHERE uid = $1 AND role = $2`,
+      [api_key, user]
     );
-    if (admin.rowCount == 0) {
-      res.send({ admin, message: `user can't access api` });
-    } else {
+    if (admin.rowCount > 0) {
       next();
+    } else {
+      res.send({ message: `user can't access api` });
     }
   },
 };
